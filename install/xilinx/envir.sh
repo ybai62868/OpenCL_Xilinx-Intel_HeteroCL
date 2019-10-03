1fbbfe8f7a2009839a851acc0ffa725bf465f105
ybai62868 <ybai62868@gmail.com>
2019-10-03 15:46:23 -0400

    [Add] Add the SDAccel set-up scripts

---
+++ install/xilinx/envir.sh
@@ -0,0 +1,4 @@
+export XILINXD_LICENSE_FILE="/scratch/xilinx/sdaccel-zhang-05.lic"
+source /scratch/xilinx/SDx/2017.1/settings64.sh 
+
+
---
+++ install/xilinx/install_board.sh
@@ -0,0 +1,4 @@
+xbinst -f xilinx:adm-pcie-7v3:1ddr:3.0 -d .
+cd xbinst
+./install.sh
+
